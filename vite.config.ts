4f4eae09e5a98f3a67b0a27dcb9c905d43bd9feb
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2026-09-23 22:40:26 +0200

    small adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // @ts-expect-error type error without @types/node package
 import process from "node:process";
-import tailwindcss from "@tailwindcss/vite"
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [
     vue(),
-    tailwindcss(),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
