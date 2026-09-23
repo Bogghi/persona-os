f601d0a34e24e11a8de8dcbffe3f192eaec8402d
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2026-09-23 23:25:25 +0200

    added some structure for the sidebar

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,18 @@
 <template>
   <main>
     <div class="container">
-      <div class="main-container"></div>
+      <div class="side-bar">
+        <div class="element">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="div"></div>
+        <div class="element">
+          <i class="fa-solid fa-house"></i>
+        </div>
+      </div>
+      <div class="main-container">
+        <!-- in here I will be placing the content of the app --> 
+      </div>
     </div>
   </main>
 </template>
