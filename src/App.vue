ff60b3b6d51065cd67ec8004c95877612201f410
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2026-09-24 11:37:57 +0200

    implemented sidebar element with parent control

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import SideBarButton from './components/shared/SideBarButton.vue';
+
+const activeIndex = ref(0);
+const sections = [
+  {
+    index: 0,
+    icon: "fa-regular fa-square-check"
+  },
+];
+
 </script>
 
 <template>
   <main>
     <div class="container">
       <div class="side-bar">
-        <div class="element">
-          <i class="fa-solid fa-bars"></i>
-        </div>
-        <div class="div"></div>
-        <div class="element">
-          <i class="fa-solid fa-house"></i>
-        </div>
+        <SideBarButton v-for="section in sections" :is-active="activeIndex === section.index" :callback="() => activeIndex = section.index" :icon="section.icon" />
       </div>
       <div class="main-container">
         <!-- in here I will be placing the content of the app --> 
---
+++ src/components/shared/SideBarButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  isActive: boolean;
+  callback: () => void;
+  icon: String;
+}>();
+</script>
+
+<template>
+  <div class="element" :class="{ active: isActive }" @click="callback">
+    <i :class="icon"></i>
+  </div>
+</template>
